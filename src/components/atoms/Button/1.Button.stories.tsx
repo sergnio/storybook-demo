cc88eb35d4bebb405d49fdbf4b9400e01f6017f5
Sergio Najera <sergio.najera@target.com>
2020-08-06 11:08:51 -0500

    Added comments

---
+++ src/components/atoms/Button/1.Button.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "./Button";
+
+// Simple Button
+
+// Button with Color
+
+// Button w/ click handler
+
+// Button with color knob
+
+// Button with text knob
